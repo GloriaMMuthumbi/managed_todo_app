ddc0ac78c7a156fdad0077f00809be43f70e4bce
charlie-black <boompiccasso@gmail.com>
2023-01-22 13:54:18 +0300

    splash screen overflow fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,28 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:managed_app/screens/splashscreen.dart';
-import 'package:managed_app/screens/welcome_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        duration: 15000,
-        splash: Column(
-          children: [
-            Image.asset('assets/images/logo.png',
-            width: 239,
-            height: 189,
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text("Managed",
-              style: TextStyle(fontSize: 28, fontFamily: 'Aclonica',),),
-            )
-          ],
-        ),
-          nextScreen: WelcomeScreen(),
-      splashTransition: SplashTransition.fadeTransition,
-      ),
-    );
+    return Sizer(builder: (context, orientation, screenType) {
+      return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+      );
+    });
   }
 }
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,34 +1,69 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:managed_app/screens/welcome_screen.dart';
+import 'package:sizer/sizer.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _navigatetowelcome(); //comment this out to work on app without loading splash screen
+    startTime();
   }
 
-  _navigatetowelcome() async {
-    await Future.delayed(Duration(milliseconds: 3500), () {});
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>WelcomeScreen()));
+  navigationPage() async {
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => WelcomeScreen()),
+        (Route<dynamic> route) => false);
+  }
+
+  startTime() async {
+    var _duration = const Duration(seconds: 5);
+    return Timer(_duration, navigationPage);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Image.asset('assets/images/logo.png',
-            width: 239,
-            height: 189,
+        body: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: const LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.centerRight,
+          colors: [Colors.white, Colors.white],
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 35.h,
           ),
-            )
-        );
+          Image.asset(
+            "assets/images/logo.png",
+            height: 200,
+            width: 200,
+          ),
+          const Center(
+              child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Managed",
+              style: TextStyle(
+                fontSize: 28,
+                fontFamily: 'Aclonica',
+              ),
+            ),
+          )),
+        ],
+      ),
+    ));
   }
 }
