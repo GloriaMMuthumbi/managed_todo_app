c3015b5e05e62f930753449876c637c60ac3c4ec
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2023-03-16 18:20:05 +0300

    Add new subtask modifications

--- lib/screens/new_task_form_screen.dart
+++ lib/screens/new_task_form_screen.dart
@@ -11,11 +11,16 @@ class NewTaskFormScreen extends StatefulWidget {
 
 class _NewTaskFormScreenState extends State<NewTaskFormScreen> {
 
-  TextEditingController dateInputController = TextEditingController();
+  final TextEditingController dateInputController = TextEditingController();
+  final TextEditingController subtaskController = TextEditingController();
+
+  bool? isActive = false;
+  bool? isSelected= false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(8.h),
           child: AppBar(
@@ -27,137 +32,214 @@ class _NewTaskFormScreenState extends State<NewTaskFormScreen> {
             ),
           ),
         ),
-        body: Center(
+        body: Container(
           child: Padding(
             padding: const EdgeInsets.only(right: 25.0, left: 25.0),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Padding(
-                padding: EdgeInsets.only(bottom: 4.0,),
-                child: Text("New Tasks",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Inter',
-                    fontSize: 24,
-                    fontWeight: FontWeight.w800,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 4.0,),
+                    child: Text("New Tasks",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800,
                       ),
                     ),
                   ),
-                SizedBox (height: 3.h,),
-                const TextField(
-                  decoration: InputDecoration(
+                  SizedBox (height: 3.h,),
+                  const TextField(
+                    decoration: InputDecoration(
                       border: InputBorder.none,
                       filled: true,
                       fillColor: Color(0xffF0F0F0),
                       labelText: 'Task Title',
-                      labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 15, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
-                      //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                      labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
+                        //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox (height: 3.h,),
-                const TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Color(0xffF0F0F0),
-                    labelText: 'Task Description',
-                    labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 15, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
-                      //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                  SizedBox (height: 3.h,),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Color(0xffF0F0F0),
+                      labelText: 'Task Description',
+                      labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
+                        //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                      ),
                     ),
                   ),
-                    //maxLines: 5,
-                    //minLines: 1
-                ),
-                SizedBox (height: 3.h,),
-                TextField(
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Color(0xffF0F0F0),
-                    labelText: 'Date Due',
-                    labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 15, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
-                      //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                  SizedBox (height: 3.h,),
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Color(0xffF0F0F0),
+                      labelText: 'Select Date Due',
+                      labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),
+                        //labelStyle: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Color(0xffB9B9B9),
+                      ),
                     ),
+                    controller: dateInputController,
+                    readOnly: true,
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2050),
+                          builder: (context, child) {
+                            return Theme(
+                              data: Theme.of(context).copyWith(
+                                  colorScheme: const ColorScheme.light(
+                                    primary: Color(0xffFFAC48),
+                                    onPrimary: Colors.white,
+                                    onSurface: Color(0xff000000),
+                                  )
+                              ),
+                              child: child!,
+                            );
+                          });
+                      if(pickedDate != null){
+                        dateInputController.text = DateFormat.yMMMMEEEEd().format(pickedDate).toString();
+                      }
+                    },
                   ),
-                  controller: dateInputController,
-                  readOnly: true,
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime(2050),
-                        builder: (context, child) {
-                          return Theme(
-                            data: Theme.of(context).copyWith(
-                                colorScheme: const ColorScheme.light(
-                                  primary: Color(0xffFFAC48),
-                                  onPrimary: Colors.white,
-                                  onSurface: Color(0xff000000),
+                  SizedBox (height: 3.h,),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: const [
+                              Text('Subtasks:',
+                                  style: TextStyle(
+                                    fontFamily: 'Inter',
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black,
+                                  )
+                              ),
+                            ],
+                          ),
+                          SizedBox (height: 2.h,),
+                          CheckboxListTile(
+                            controlAffinity: ListTileControlAffinity.leading,
+                            title: const Text('Subtask 1',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                  //decoration: isSelected ? TextDecoration.lineThrough : null,
                                 )
                             ),
-                            child: child!,
-                          );
-                        });
-                    if(pickedDate != null){
-                      dateInputController.text = DateFormat.yMMMMEEEEd().format(pickedDate).toString();
-                    }
-                  },
-                ),
-                SizedBox (height: 3.h,),
-                const Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Text('Subtasks',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xffB9B9B9),
-                      )
-                  ),
-                ),
-                SizedBox (height: 2.h,),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: OutlinedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.add),
-                      style: OutlinedButton.styleFrom(
-                        foregroundColor: const Color(0xffFFAC48),
-                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8))),
-                          side: const BorderSide(
-                          color: Color(0xffFFAC48),
-                          width: 2,
-                        )
+                            value: isSelected,
+                            onChanged: (value){
+                              setState(() {
+                                isSelected = value;
+                              });
+                            },
+                            activeColor: const Color(0xffFFAC48),
+                            checkColor: Colors.white,
+                          ),
+                          Align(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                  children: [
+                                    isActive==true? Column(
+                                      children: [
+                                        TextField(
+                                          decoration: const InputDecoration(
+                                            border: InputBorder.none,
+                                            isDense: true,
+                                            filled: true,
+                                            fillColor: Color(0xffF0F0F0),
+                                            labelText: 'Enter Subtask',
+                                            labelStyle: TextStyle(fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600, color: Color(0xffB9B9B9),),
+                                          ),
+                                          controller: subtaskController,
+                                        ),
+                                        TextButton.icon(
+                                          onPressed: () {
+                                            setState(() {
+                                              isActive = true;
+                                            });
+                                          },
+                                          icon: const Icon(Icons.add,
+                                            color: Color(0xffFFAC48),),
+                                          label: const Text("Add Subtask",
+                                            style: TextStyle(
+                                                fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600,
+                                                color:Color(0xffA7A7A7)
+                                            ),),
+                                        ),
+                                      ],
+                                    ):
+                                    TextButton.icon(
+                                      onPressed: () {
+                                        setState(() {
+                                          isActive = true;
+                                        });
+                                      },
+                                      icon: const Icon(Icons.add,
+                                        color: Color(0xffFFAC48),),
+                                      label: const Text("Add Subtask",
+                                        style: TextStyle(
+                                            fontFamily: 'Inter', fontSize: 14, fontWeight: FontWeight.w600,
+                                            color:Color(0xffA7A7A7)
+                                        ),),
+                                    ),
+                                  ]
+                              )
+                          ),
+                        ],
                       ),
-                    label: const Text('Add a subtask',
-                    style: TextStyle(
-                        color:Color(0xffA7A7A7)
                     ),
-                    )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: TextButton (
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xffFFAC48),
+                            padding: const EdgeInsets.fromLTRB(25, 15, 25, 15),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            )
+                        ), child:  Text("Save".toUpperCase(),
+                        style: const TextStyle(color: Colors.white, fontFamily: 'Inter', fontWeight: FontWeight.w800, fontSize: 14,),
+                      ),
+                      ),
+                    ),
                   )
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: TextButton (
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                        backgroundColor: const Color(0xffFFAC48),
-                        padding: const EdgeInsets.fromLTRB(25, 15, 25, 15),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)
-                        )
-                    ), child:  Text("Save".toUpperCase(),
-                    style: const TextStyle(color: Colors.white, fontFamily: 'Inter', fontWeight: FontWeight.w800, fontSize: 14,),
-                  ),
-                  ),
-                )
-              ]
+                ]
             ),
+          ),
           )
-        )
     );
   }
 }
+
+
+
+
+/*IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              isActive = false;
+                                            });
+                                          },
+                                          icon: const Icon(
+                                            Icons.delete,
+                                            color: Color(0xffea0000),
+                                            size: 22,
+                                          ),
+                                      ),*/
