21ab0f4023c7f00af703251e3e500e348b2a9eb3
king <infor.samson@gmail.com>
2023-06-06 20:30:01 +0300

    Version 1: Format code and remove underscore (#1)
    * Remove the underscore
    
    * Format the code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,18 +4,16 @@ import 'package:sizer/sizer.dart';
 import 'new_task_form_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-
-    TabController _tabController = TabController(length: 3, vsync: this);
+    TabController tabController = TabController(length: 3, vsync: this);
 
     return Scaffold(
       appBar: PreferredSize(
@@ -36,15 +34,13 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
                       onPressed: () {},
                       icon: const Icon(
                         Icons.search,
-                      )
-                  ),
+                      )),
                   IconButton(
                       iconSize: 3.5.h,
                       onPressed: () {},
                       icon: const Icon(
                         Icons.notifications_none_outlined,
-                      )
-                  )
+                      ))
                 ],
               ),
             ),
@@ -55,92 +51,99 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
         child: Column(
           children: [
             Container(
-              child: const Padding(
-                padding: EdgeInsets.only(bottom: 4.0,),
-                child: Text("My Tasks",
+                child: const Padding(
+              padding: EdgeInsets.only(
+                bottom: 4.0,
+              ),
+              child: Text(
+                "My Tasks",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontFamily: 'Inter',
                   fontSize: 24,
                   fontWeight: FontWeight.w800,
-                ),),
-              )
-            ),
+                ),
+              ),
+            )),
             Container(
-              child: TabBar(
-                controller: _tabController,
-                labelColor: Colors.black,
-                labelStyle: const TextStyle(
-                  fontSize: 15.0,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w600,
-                ),
-                unselectedLabelColor: const Color(0xffA7A7A7),
-                unselectedLabelStyle: const TextStyle(
-                  fontSize: 15.0,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w500,
-                ),
-                indicatorColor: const Color(0xffFFAC48),
-                tabs: const <Tab> [
-                  Tab(text: "To-do"),
-                  Tab(text: "Due Soon"),
-                  Tab(text: "Completed"),
-                ],
-              )
-            ),
+                child: TabBar(
+              controller: tabController,
+              labelColor: Colors.black,
+              labelStyle: const TextStyle(
+                fontSize: 15.0,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+              ),
+              unselectedLabelColor: const Color(0xffA7A7A7),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 15.0,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w500,
+              ),
+              indicatorColor: const Color(0xffFFAC48),
+              tabs: const <Tab>[
+                Tab(text: "To-do"),
+                Tab(text: "Due Soon"),
+                Tab(text: "Completed"),
+              ],
+            )),
             Container(
               width: 100.w,
               height: 77.h,
               child: TabBarView(
-                controller: _tabController,
+                controller: tabController,
                 children: [
                   Center(
-                     child: Column(
-                       children: [
-                         const Expanded(
-                           child: Center(
-                             child: Text("You currently have no tasks to do",
+                    child: Column(
+                      children: [
+                        const Expanded(
+                          child: Center(
+                            child: Text(
+                              "You currently have no tasks to do",
                               style: TextStyle(
                                 fontFamily: 'Inter',
                                 fontWeight: FontWeight.w500,
                                 fontSize: 15.0,
                                 color: Color(0xffA7A7A7),
                               ),
-                             ),
-                           ),
-                         ),
-                         Padding(
-                           padding: const EdgeInsets.only(right: 15.0, bottom: 15.0),
-                           child: Align(
-                             alignment: Alignment.bottomRight,
-                             child: ElevatedButton(
-                               onPressed: () {
-                                 Navigator.push(
-                                     context,
-                                     MaterialPageRoute(builder: (context) => const NewTaskFormScreen()),
-                                 );
-                               },
-                               style: ElevatedButton.styleFrom(
-                                   elevation: 5.0,
-                                   backgroundColor: Color(0xffFFAC48),
-                                   shape: CircleBorder(),
-                                 padding: EdgeInsets.all(2.h),
-                                   ),
-                               child: const Icon (
-                                 Icons.add,
-                                 size: 30,
-                               ),
-                             ),
-                           ),
-                         )
-                       ],
-                     ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(right: 15.0, bottom: 15.0),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const NewTaskFormScreen()),
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                elevation: 5.0,
+                                backgroundColor: Color(0xffFFAC48),
+                                shape: CircleBorder(),
+                                padding: EdgeInsets.all(2.h),
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                size: 30,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                   const Center(
                     child: Padding(
                       padding: EdgeInsets.only(bottom: 85.0),
-                      child: Text("You currently have no tasks due soon",
+                      child: Text(
+                        "You currently have no tasks due soon",
                         style: TextStyle(
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w500,
@@ -153,7 +156,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
                   const Center(
                     child: Padding(
                       padding: EdgeInsets.only(bottom: 85.0),
-                      child: Text("You currently have no completed tasks",
+                      child: Text(
+                        "You currently have no completed tasks",
                         style: TextStyle(
                           fontFamily: 'Inter',
                           fontWeight: FontWeight.w500,
